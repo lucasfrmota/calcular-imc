8bbbe337876c4b9c068af215a41a87a722ac54d6
Lucas Freire <lucasfrmota@gmail.com>
2023-04-19 17:10:56 -0300

    Erros tratados
    -Removido o Header
    -Removido espaçamento entre os nomes

--- imc.py
+++ imc.py
@@ -7,9 +7,9 @@ df['Altura (m)'] = df['Altura (m)'].str.replace(',', '.').astype(float)
 df['Primeiro Nome'] = df['Primeiro Nome'].str.capitalize()
 
 df['IMC'] = df[['Peso (kg)', 'Altura (m)']].apply(lambda x: round(x['Peso (kg)']/x['Altura (m)']**2,2), axis = 1)
-df['Nome completo'] = df.apply(lambda x: x['Primeiro Nome'] + ' ' +  x['Sobrenomes'], axis = 1)
+df['Nome completo'] = df.apply(lambda x: x['Primeiro Nome'].strip() + ' ' +  x['Sobrenomes'].strip(), axis = 1)
 df['Nome completo'] = df['Nome completo'].str.upper()
+
 df_final = df[['Nome completo', 'IMC']]
 
-
-df_final.to_csv('LUCASFREIREMOTA.txt', index=False)
+df_final.to_csv('LUCASFREIREMOTA.txt', header = False, index=False)
