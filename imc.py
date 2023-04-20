7b2976c4b09cae0b00058fc9f06076122677b0f2
Lucas Freire <lucasfrmota@gmail.com>
2023-04-20 12:17:03 -0300

    Mais alguns tratamentos de erros

--- imc.py
+++ imc.py
@@ -1,15 +1,18 @@
 import pandas as pd
 
-df = pd.read_csv('DATASET.CSV', delimiter=';')
+df = pd.read_csv('DATASET.CSV', sep=';')
 
 df['Peso (kg)'] = df['Peso (kg)'].str.replace(',', '.').astype(float)
 df['Altura (m)'] = df['Altura (m)'].str.replace(',', '.').astype(float)
 df['Primeiro Nome'] = df['Primeiro Nome'].str.capitalize()
 
 df['IMC'] = df[['Peso (kg)', 'Altura (m)']].apply(lambda x: round(x['Peso (kg)']/x['Altura (m)']**2,2), axis = 1)
-df['Nome completo'] = df.apply(lambda x: x['Primeiro Nome'].strip() + ' ' +  x['Sobrenomes'].strip(), axis = 1)
+df['Nome completo'] = df.apply(lambda x: x['Primeiro Nome'].strip() + ' ' +  x['Sobrenomes'], axis = 1)
 df['Nome completo'] = df['Nome completo'].str.upper()
 
 df_final = df[['Nome completo', 'IMC']]
 
-df_final.to_csv('LUCASFREIREMOTA.txt', header = False, index=False)
+separaSobrenome = df['Sobrenomes'].str.split()
+print(separaSobrenome.str.capitalize())
+
+#df_final.to_csv('[lucasFreireMota].txt', header = False, index=False, sep=" ")
